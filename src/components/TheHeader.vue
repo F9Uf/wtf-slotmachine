21eb8a4fe7bfe17cab1f2f61a159b4b78a25798e
F9Uf <atoseka@gmail.com>
2021-05-01 14:37:17 +0700

    create TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="w-full py-5 shadow-normal px-10 flex justify-between items-center">
+    <div class="left-header">
+      <router-link to="/" class="flex items-center">
+        <img src="../assets/logo.png" class="h-8 mr-5" alt="">
+        WTF Slot Machine
+      </router-link>
+    </div>
+    <div class="right-header">
+      <div class="has-wallet" v-if="isLoginWallet">
+        Your wallet:
+        <button class="px-4 py-1 bg-blue-300 rounded-md ml-3 focus:outline-none">{{ shortWalletAddress }}</button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoginWallet: true,
+      walletAddress: '0x100000000000000000000000000000000000DEAD'
+    }
+  },
+  computed: {
+    shortWalletAddress() {
+      return `${this.walletAddress.slice(0,6)}...${this.walletAddress.slice(-4)}`
+    }
+  }
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    boxShadow: {
+      'normal': '0px 1px 30px 0px rgba(0,0,0,0.1)',
+    }
   },
   variants: {
     extend: {},
