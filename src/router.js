bc43017372a8451c0913c0bbeafba94a36556d23
F9Uf <atoseka@gmail.com>
2021-05-01 23:04:22 +0700

    change vite -> vue-cli

--- src/router.js
+++ src/router.js
@@ -1,4 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import Vue from 'vue'
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -13,9 +16,10 @@ const routes = [
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: routes
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
 
 export default router
