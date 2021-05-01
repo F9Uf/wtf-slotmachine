b84addc23f79ced2e630df14e276e8bcdcbcfd1f
F9Uf <atoseka@gmail.com>
2021-05-01 15:29:43 +0700

    create Button component

---
+++ src/components/common/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    :class="`
+      focus:outline-none w-full ${displayStyle} rounded-lg text-white py-2 px-3 transition-all
+      ${type === 'disabled' ? 'cursor-not-allowed' : 'cursor-pointer'}
+      ${type === 'disabled' ? 'hover:opacity-100' : 'hover:opacity-90'}
+    `"
+  >
+    Swap
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: false
+    }
+  },
+  computed: {
+    displayStyle() {
+      const avaliableColor = ['disabled', 'dark', 'secondary', 'primary', 'light'];
+      if (avaliableColor.indexOf(this.type) >= 0) {
+        return `bg-${this.type}`
+      }
+      return 'bg-primary'
+    }
+  }
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,19 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#007AC2',
+        'secondary': '#18C2E6',
+        'dark': '#004673',
+        'disabled': '#B8B8B8',
+        'light': '#8CE6FF'
+      }
+    },
     boxShadow: {
       'normal': '0px 1px 40px 0px rgba(0,0,0,0.05)',
       'card': '4px 4px 40px 0px rgb(0,0,0,0.05)'
-    }
+    },
   },
   variants: {
     extend: {},
