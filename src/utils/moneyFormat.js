e41e90a1b5c6014043033733a4cf87968d2daa5b
F9Uf <atoseka@gmail.com>
2021-05-02 13:57:19 +0700

    create function to convert wei to eth

--- src/utils/moneyFormat.js
+++ src/utils/moneyFormat.js
@@ -7,3 +7,9 @@ export const numberToMoney = (numberOfMoney, format) => {
   var re = '\\d(?=(\\d{' + (3) + '})+' + (format > 0 ? '\\.' : '$') + ')';
   return numberOfMoney.toFixed(Math.max(0, ~~format)).replace(new RegExp(re, 'g'), '$&,');
 }
+
+export const strWeiToEth = (strWei) => {
+  const wei = Number(strWei);
+  const eth = wei / Math.pow(10, 18)
+  return numberToMoney(eth, 2)
+}
