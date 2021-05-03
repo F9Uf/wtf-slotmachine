74970a4085efffb4a137090336121df6f7e1151f
pongpat11 <pongpatsitiputa@gmail.com>
2021-05-03 20:05:56 +0700

    history, slot

--- src/views/SlotPage.vue
+++ src/views/SlotPage.vue
@@ -3,30 +3,46 @@
     <div class="history col-start-1 col-span-3">
       <Card>
         <Title>History</Title>
+        <div class="grid grid-flow-row auto-rows-max">
+          <div class="history my-4" v-for="history in histories" :key="history.n">
+            <div class="grid grid-cols-4 gap-2">
+              <div>#{{history.n}}</div>
+              <div>{{history.c1}}</div>
+              <div>{{history.c2}}</div>
+              <div>{{history.c3}}</div>
+            </div> 
+          </div>
+        </div>
       </Card>
     </div>
     <div class="slot-machine col-start-4 col-span-6">
       <Card class="grid grid-rows-3 grid-flow-col">
         <Title>Slot Machine</Title>
-        <div class="slot grid grid-cols-5 gap-1 justify-items-center bg-blue-900 h-40">
-          <img
-            class="col-start-2 col-span-1"
-            src="../assets/currency/btc_slot.png"
-            alt=""
-            style="align"
-          />
-          <img
-            class="col-start-3 col-span-1"
-            src="../assets/currency/btc_slot.png"
-            alt=""
-          />
-          <img
-            class="col-start-4 col-span-1"
-            src="../assets/currency/btc_slot.png"
-            alt=""
-          />
+        <div class="slot grid grid-cols-11">
+          <div
+            class="col-start-1 col-span-3 grid place-items-center bg-blue-900 h-40 rounded-lg"
+          >
+            <img src="../assets/currency/btc_slot.png" alt="" />
+          </div>
+          <div
+            class="col-start-5 col-span-3 grid place-items-center bg-blue-900 h-40 rounded-lg"
+          >
+            <img src="../assets/currency/btc_slot.png" alt="" />
+          </div>
+          <div
+            class="col-start-9 col-span-3 grid place-items-center bg-blue-900 h-40 rounded-lg"
+          >
+            <img src="../assets/currency/btc_slot.png" alt="" />
+          </div>
         </div>
-        <Button :type="getBtnDisplay.type">{{ getBtnDisplay.text }}</Button>
+        <div class="bottom mt-10">
+          <div class="float-right mb-5">
+            Balance: 10 WTF (~10,000$)
+          </div>
+          <Button :type="getBtnDisplay.type">{{
+            getBtnDisplay.text
+          }}</Button>
+        </div>
       </Card>
     </div>
     <div class="claim-and-rules col-start-10 col-span-3">
@@ -121,6 +137,24 @@ import Description from "../components/common/Description.vue";
 
 export default {
   components: { Card, Title, Button, Description },
+  data() {
+    return {
+      histories: [
+        {
+          n: 1,
+          c1: "btc",
+          c2: "btc",
+          c3: "btc"
+        },
+        {
+          n: 2,
+          c1: "btc",
+          c2: "btc",
+          c3: "btc"
+        }
+      ]
+    }
+  },
   computed: {
     getBtnDisplay() {
       if (this.web3Type === "OK") return { type: "primary", text: "Swap" };
